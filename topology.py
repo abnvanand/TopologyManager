b5684cb490ac66f1a4c46f9ab38cb50d54a55699
Abhinav Anand <abnvanand@gmail.com>
2020-04-23 22:54:16 +0530

    Redploy only admin services

--- topology.py
+++ topology.py
@@ -17,22 +17,30 @@ def poll():
     s = Session()
 
     s.execute('SET search_path TO "ias-schema"')
-    services = s.query(Service).filter_by(status='alive').filter_by(redeployRequest='false').all()
+    services = s.query(Service).filter_by(status='alive')\
+        .filter_by(redeployRequest='false')\
+        .all()
 
     print("services list:", services)
     for service in services:
         alive = healthCheck.check_health(service)
         if not alive:
-            print("Service ", service.serviceId, "is down.")
-            redeployRequest = RedeployRequest(service.serviceId,
-                                              service.serviceName,
-                                              service.username,
-                                              service.applicationName)
 
-            request_sent = rest_handler.redeploy_service(redeployRequest)
-            service.redeployRequest = 'true' if request_sent else 'false'
-            s.add(service)
-            s.commit()
+            if service.username != "admin":
+                service.status = "stopped"
+                s.add(service)
+                s.commit()
+            else:
+                print("Service ", service.serviceId, "is down.")
+                redeployRequest = RedeployRequest(service.serviceId,
+                                                  service.serviceName,
+                                                  service.username,
+                                                  service.applicationName)
+
+                request_sent = rest_handler.redeploy_service(redeployRequest)
+                service.redeployRequest = 'true' if request_sent else 'false'
+                s.add(service)
+                s.commit()
         else:
             print("Service ", service.serviceId, "is alive.")
 
